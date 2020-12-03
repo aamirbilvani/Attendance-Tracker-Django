0eb45cc70b4b9f7f152c11f32e3e7d1e29d5e3ba
Shayan Samad <shayan.samad@gatron-novatex.com>
2020-12-03 12:43:11 +0500

    static_root settings

--- attendanceTracker/attendanceTracker/settings.py
+++ attendanceTracker/attendanceTracker/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,4 +130,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
