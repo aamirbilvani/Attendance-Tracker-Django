e582ff2b20fe52351f8f86f36612492b11b90dd7
Shayan Samad <shayan.samad@gatron-novatex.com>
2020-12-04 10:43:26 +0500

    static settings

--- attendanceTracker/attendanceTracker/settings.py
+++ attendanceTracker/attendanceTracker/settings.py
@@ -133,3 +133,6 @@ USE_TZ = True
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+
